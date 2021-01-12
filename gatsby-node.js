5a7eb6e50790e58016d4689969399ec4c61ed710
Ben Townshend <ben.townshend@craftworkdigital.com>
2021-01-12 20:08:48 +0000

    fixed the homepage problem

--- gatsby-node.js
+++ gatsby-node.js
@@ -25,13 +25,13 @@ exports.createPages = ({ graphql, actions }) => {
     `).then(result => {
         const pages = result.data.allWpPage.edges.filter(p => [
             // TODO: Add any pages that should have a custom template here
-            '/home/',
+            // '/home/',
             '/news/',
         ].includes(p.node.uri) === false)
 
         pages.forEach(({ node }) => {
             createPage({
-                path: node.uri,
+                path: node.uri === '/home/' ? '/' : node.uri,
                 component: path.resolve(`./src/templates/page.js`),
                 context: {
                     slug: node.slug,
